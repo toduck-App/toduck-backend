8cb80240ec2741ead374bfc094cfb9ed27eb93f9
Seol_JY <wlsdud5654@gmail.com>
2024-07-19 02:59:48 +0900

    chore: 민감정보 관리용 submodule 연동

--- src/main/java/im/toduck/ToduckBackendApplication.java
+++ src/main/java/im/toduck/ToduckBackendApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ToduckBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ToduckBackendApplication.class, args);
 	}
-
 }
