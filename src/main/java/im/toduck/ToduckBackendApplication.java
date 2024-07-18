52f6ee98281417023808f19eb151f8769c45f64d
Seol_JY <wlsdud5654@gmail.com>
2024-07-18 20:44:08 +0900

    feat: 초기 프로젝트 생성

---
+++ src/main/java/im/toduck/ToduckBackendApplication.java
@@ -0,0 +1,13 @@
+package im.toduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToduckBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToduckBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/im/toduck/ToduckBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package im.toduck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToduckBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
