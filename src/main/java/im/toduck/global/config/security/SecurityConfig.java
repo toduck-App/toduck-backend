d90387a0251838326b13f6d26c3eee9e6e228c80
Junad-Park <qkrwnsgk1624@icloud.com>
2024-08-02 06:09:22 +0900

    feat: security, filter 관련 작성

---
+++ src/main/java/im/toduck/global/config/security/SecurityConfig.java
@@ -0,0 +1,74 @@
+package im.toduck.global.config.security;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.AbstractRequestMatcherRegistry;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import im.toduck.global.security.filter.JwtAuthenticationFilter;
+import im.toduck.global.security.filter.JwtExceptionFilter;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+	private static final String[] SWAGGER_ENDPOINTS = {"/swagger-ui/**", "/swagger"};
+	private static final String[] PUBLIC_ENDPOINTS = {"/", "/error"};
+	private static final String[] ANONYMOUS_ENDPOINTS = {"/v1/auth/**"};
+
+	private final CorsConfigurationSource corsConfigurationSource;
+	private final AccessDeniedHandler accessDeniedHandler;
+	private final AuthenticationEntryPoint authenticationEntryPoint;
+
+	private final DaoAuthenticationProvider daoAuthenticationProvider;
+	private final JwtExceptionFilter jwtExceptionFilter;
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return defaultSecurity(http)
+			.cors(cors -> cors.configurationSource(corsConfigurationSource))
+			.authorizeHttpRequests(
+				request -> defaultAuthorizeHttpRequests(request)
+					.requestMatchers(SWAGGER_ENDPOINTS).permitAll()
+					.anyRequest().authenticated()
+			).build();
+	}
+
+	private HttpSecurity defaultSecurity(HttpSecurity http) throws Exception {
+		return http.httpBasic(AbstractHttpConfigurer::disable)
+			.csrf(AbstractHttpConfigurer::disable)
+			.sessionManagement(sessionManagement ->
+				sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.formLogin(AbstractHttpConfigurer::disable)
+			.logout(AbstractHttpConfigurer::disable)
+
+			.authenticationProvider(daoAuthenticationProvider)
+			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+			.addFilterBefore(jwtExceptionFilter, JwtAuthenticationFilter.class)
+
+			.exceptionHandling(exception -> exception
+				.accessDeniedHandler(accessDeniedHandler)
+				.authenticationEntryPoint(authenticationEntryPoint));
+	}
+
+	private AbstractRequestMatcherRegistry
+		<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizedUrl> defaultAuthorizeHttpRequests(
+		AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry auth) {
+		return auth.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+			.requestMatchers(PUBLIC_ENDPOINTS).permitAll()
+			.requestMatchers(ANONYMOUS_ENDPOINTS).anonymous();
+	}
+}
---
+++ src/main/java/im/toduck/global/security/handler/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,35 @@
+package im.toduck.global.security.handler;
+
+import static im.toduck.global.exception.ExceptionCode.*;
+
+import java.io.IOException;
+
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import im.toduck.global.presentation.ApiResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+	private static final String CHARACTER_ENCODING = "UTF-8";
+	private final ObjectMapper objectMapper;
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+		AuthenticationException authException)
+		throws IOException, ServletException {
+
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setCharacterEncoding(CHARACTER_ENCODING);
+		response.setStatus(EMPTY_ACCESS_TOKEN.getHttpStatus().value());
+		objectMapper.writeValue(response.getWriter(), ApiResponse.createError(EMPTY_ACCESS_TOKEN));
+	}
+
+}
