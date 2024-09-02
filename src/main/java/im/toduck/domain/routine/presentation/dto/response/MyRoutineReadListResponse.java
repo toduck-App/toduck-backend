806f4041924ba78d822b4c6c3c3915605e6e97d2
Seol_JY <wlsdud5654@gmail.com>
2024-09-02 21:40:15 +0900

    fix: 일시 관련 Serialize 등록

--- src/main/java/im/toduck/domain/routine/presentation/dto/response/MyRoutineReadListResponse.java
+++ src/main/java/im/toduck/domain/routine/presentation/dto/response/MyRoutineReadListResponse.java
@@ -4,12 +4,19 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Schema(description = "본인 루틴 목록 응답 DTO")
 @Builder
 public record MyRoutineReadListResponse(
+	@JsonSerialize(using = LocalDateSerializer.class)
+	@JsonFormat(pattern = "yyyy-MM-dd")
 	@Schema(description = "조회 날짜", example = "2024-08-31")
 	LocalDate queryDate,
 
@@ -26,6 +33,8 @@ public record MyRoutineReadListResponse(
 		@Schema(description = "루틴 색상(null 이면 없는 색상)", example = "#FCDCDF")
 		String color,
 
+		@JsonSerialize(using = LocalTimeSerializer.class)
+		@JsonFormat(pattern = "HH:mm")
 		@Schema(description = "루틴 시간(null 이면 종일 루틴)", example = "14:30")
 		LocalTime time,
 
