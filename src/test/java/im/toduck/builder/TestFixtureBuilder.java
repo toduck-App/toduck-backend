df3309ec4174c786eab9ce5c701b8109b3eff376
Seol_JY <wlsdud5654@gmail.com>
2024-09-06 04:17:52 +0900

    test: routine 도메인 관련 테스트 준비

--- src/test/java/im/toduck/builder/TestFixtureBuilder.java
+++ src/test/java/im/toduck/builder/TestFixtureBuilder.java
@@ -3,6 +3,8 @@ package im.toduck.builder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import im.toduck.domain.routine.persistence.entity.Routine;
+import im.toduck.domain.routine.persistence.entity.RoutineRecord;
 import im.toduck.domain.user.persistence.entity.User;
 import im.toduck.infra.redis.phonenumber.PhoneNumber;
 
@@ -18,6 +20,14 @@ public class TestFixtureBuilder {
 
 	public PhoneNumber buildPhoneNumber(final PhoneNumber phoneNumber) {
 		return bs.phoneNumberRepository().save(phoneNumber);
+	}
+
+	public Routine buildRoutine(final Routine routine) {
+		return bs.routineRepository().save(routine);
+	}
+
+	public RoutineRecord buildRoutineRecord(final RoutineRecord routineRecord) {
+		return bs.routineRecord().save(routineRecord);
 	}
 
 }
