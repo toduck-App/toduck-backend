038c85bfcdad0186ea46453c454fdc6d00f0cf24
Kkang <aorl2313@naver.com>
2024-09-10 19:06:06 +0900

    chore: feign 의존성 빌드파일에 추가 및 feign 전역 설정

--- src/main/java/im/toduck/ToduckBackendApplication.java
+++ src/main/java/im/toduck/ToduckBackendApplication.java
@@ -2,8 +2,10 @@ package im.toduck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ToduckBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ToduckBackendApplication.class, args);
