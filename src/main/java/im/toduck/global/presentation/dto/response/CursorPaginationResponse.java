0af15a07a9fa420bd0e91cf80a17f8278fab06c5
Junad-Park <qkrwnsgk1624@icloud.com>
2024-09-12 16:50:57 +0900

    style: pagination response 개행 추가

--- src/main/java/im/toduck/global/presentation/dto/response/CursorPaginationResponse.java
+++ src/main/java/im/toduck/global/presentation/dto/response/CursorPaginationResponse.java
@@ -7,8 +7,13 @@ import lombok.Builder;
 
 @Builder
 public record CursorPaginationResponse<T>(
-	@Schema(description = "다음 페이지가 있는지 여부", example = "true") boolean hasMore,
-	@Schema(description = "다음 페이지를 위한 커서", example = "1") Long nextCursor,
-	@Schema(description = "결과 리스트", example = "{}") List<T> results
+	@Schema(description = "다음 페이지가 있는지 여부", example = "true")
+	boolean hasMore,
+
+	@Schema(description = "다음 페이지를 위한 커서", example = "1")
+	Long nextCursor,
+
+	@Schema(description = "결과 리스트", example = "{}")
+	List<T> results
 ) {
 }
