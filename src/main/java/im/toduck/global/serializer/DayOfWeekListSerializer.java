501449dca38a1d81d6103b31affc4e8550253921
Seol_JY <wlsdud5654@gmail.com>
2024-09-28 03:43:45 +0900

    feat: serializer 에 null 체크 & 코드 간소화

--- src/main/java/im/toduck/global/serializer/DayOfWeekListSerializer.java
+++ src/main/java/im/toduck/global/serializer/DayOfWeekListSerializer.java
@@ -8,14 +8,15 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
+import jakarta.validation.constraints.NotNull;
+
 public class DayOfWeekListSerializer extends JsonSerializer<List<DayOfWeek>> {
 
 	@Override
-	public void serialize(List<DayOfWeek> value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-		if (value == null || value.isEmpty()) {
-			gen.writeStartArray();
-			gen.writeEndArray();
-			return;
+	public void serialize(@NotNull List<DayOfWeek> value, JsonGenerator gen, SerializerProvider provider)
+		throws IOException {
+		if (value == null) {
+			throw new IllegalArgumentException("Value cannot be null");
 		}
 
 		gen.writeStartArray();
