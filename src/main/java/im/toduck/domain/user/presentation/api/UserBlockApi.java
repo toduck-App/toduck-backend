1bdba2262f9eb97fb3c213036f33b17bf9411923
Junad-Park <qkrwnsgk1624@icloud.com>
2024-09-30 13:44:18 +0900

    docs: 유저 차단 관련 api 문서 내용 추가

--- src/main/java/im/toduck/domain/user/presentation/api/UserBlockApi.java
+++ src/main/java/im/toduck/domain/user/presentation/api/UserBlockApi.java
@@ -6,7 +6,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import im.toduck.global.annotation.swagger.ApiErrorResponseExplanation;
 import im.toduck.global.annotation.swagger.ApiResponseExplanations;
+import im.toduck.global.annotation.swagger.ApiSuccessResponseExplanation;
+import im.toduck.global.exception.ExceptionCode;
 import im.toduck.global.presentation.ApiResponse;
 import im.toduck.global.security.authentication.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
@@ -19,7 +22,15 @@ public interface UserBlockApi {
 		summary = "유저 차단",
 		description = "지정된 유저를 차단합니다."
 	)
-	@ApiResponseExplanations
+	@ApiResponseExplanations(
+		success = @ApiSuccessResponseExplanation(
+			description = "유저 차단 성공, 빈 content 객체를 반환합니다."
+		),
+		errors = {
+			@ApiErrorResponseExplanation(exceptionCode = ExceptionCode.CANNOT_BLOCK_SELF),
+			@ApiErrorResponseExplanation(exceptionCode = ExceptionCode.ALREADY_BLOCKED),
+		}
+	)
 	ResponseEntity<ApiResponse<Map<String, Object>>> blockUser(
 		@PathVariable("blockedUserId") Long blockedUserId,
 		@AuthenticationPrincipal CustomUserDetails user
@@ -29,7 +40,14 @@ public interface UserBlockApi {
 		summary = "유저 차단 취소",
 		description = "지정된 유저의 차단을 취소합니다."
 	)
-	@ApiResponseExplanations
+	@ApiResponseExplanations(
+		success = @ApiSuccessResponseExplanation(
+			description = "유저 차단 취소 성공, 빈 content 객체를 반환합니다."
+		),
+		errors = {
+			@ApiErrorResponseExplanation(exceptionCode = ExceptionCode.NOT_FOUND_BLOCK),
+		}
+	)
 	ResponseEntity<ApiResponse<Map<String, Object>>> unblockUser(
 		@PathVariable("blockedUserId") Long blockedUserId,
 		@AuthenticationPrincipal CustomUserDetails user
