486fa50307a167ecf16c73672870f22a0a51c827
Junad-Park <qkrwnsgk1624@icloud.com>
2024-11-29 18:46:58 +0900

    feat: 댓글 좋아요 관련 entity 생성 및 수정

--- src/main/java/im/toduck/domain/social/persistence/entity/CommentLike.java
+++ src/main/java/im/toduck/domain/social/persistence/entity/CommentLike.java
@@ -2,60 +2,50 @@ package im.toduck.domain.social.persistence.entity;
 
 import java.time.LocalDateTime;
 
-import im.toduck.domain.social.persistence.vo.CommentContent;
 import im.toduck.domain.user.persistence.entity.User;
 import im.toduck.global.base.entity.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@Table(name = "comment")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Comment extends BaseEntity {
+@Table(name = "comment_likes")
+@NoArgsConstructor
+public class CommentLike extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Embedded
-	private CommentContent content;
-
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
 
-	@ManyToOne
-	@JoinColumn(name = "social_id", nullable = false)
-	private Social social;
-
-	@Column(nullable = false, columnDefinition = "int default 0")
-	private int likeCount;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "comment_id", nullable = false)
+	private Comment comment;
 
 	@Builder
-	private Comment(User user, Social social, CommentContent content) {
+	private CommentLike(User user, Comment comment) {
 		this.user = user;
-		this.social = social;
-		this.content = content;
+		this.comment = comment;
 	}
 
-	public boolean isOwner(User requestingUser) {
+	public boolean isOwner(final User requestingUser) {
 		return this.user.getId().equals(requestingUser.getId());
 	}
 
-	public boolean isInSocialBoard(Social socialBoard) {
-		return this.social.getId().equals(socialBoard.getId());
+	public boolean isForComment(final Comment comment) {
+		return this.comment.getId().equals(comment.getId());
 	}
 
 	public void softDelete() {
