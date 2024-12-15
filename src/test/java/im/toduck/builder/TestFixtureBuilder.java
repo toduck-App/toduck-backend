91fc592b87252dfced51d3021561ff4b7d7560e9
Seol_JY <wlsdud5654@gmail.com>
2024-12-16 05:20:23 +0900

    feat: 테스트를 위해 루틴 Fixture 사용시 createdAt 등 시간 관련 필드들(Audit 필드 포함)을 명시적으로 지정할 수 있도록 변경

--- src/test/java/im/toduck/builder/TestFixtureBuilder.java
+++ src/test/java/im/toduck/builder/TestFixtureBuilder.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import im.toduck.domain.routine.persistence.entity.Routine;
 import im.toduck.domain.routine.persistence.entity.RoutineRecord;
@@ -15,6 +16,7 @@ import im.toduck.domain.social.persistence.entity.SocialCategory;
 import im.toduck.domain.social.persistence.entity.SocialImageFile;
 import im.toduck.domain.user.persistence.entity.Block;
 import im.toduck.domain.user.persistence.entity.User;
+import im.toduck.fixtures.routine.RoutineWithAuditInfo;
 import im.toduck.infra.redis.phonenumber.PhoneNumber;
 
 @Component
@@ -33,6 +35,21 @@ public class TestFixtureBuilder {
 
 	public Routine buildRoutine(final Routine routine) {
 		return bs.routineRepository().save(routine);
+	}
+
+	public Routine buildRoutineAndUpdateAuditFields(final RoutineWithAuditInfo routineWithAuditInfo) {
+		Routine savedRoutine = bs.routineRepository().save(routineWithAuditInfo.getRoutine());
+
+		if (routineWithAuditInfo.requiresAudit()) {
+			ReflectionTestUtils.setField(savedRoutine, "createdAt", routineWithAuditInfo.getCreatedAt());
+			ReflectionTestUtils.setField(savedRoutine, "scheduleModifiedAt",
+				routineWithAuditInfo.getScheduleModifiedAt());
+			ReflectionTestUtils.setField(savedRoutine, "deletedAt", routineWithAuditInfo.getDeletedAt());
+		}
+
+		return bs.routineRepository()
+			.findById(savedRoutine.getId())
+			.orElseThrow(() -> new RuntimeException("루틴을 찾을 수 없음"));
 	}
 
 	public RoutineRecord buildRoutineRecord(final RoutineRecord routineRecord) {
