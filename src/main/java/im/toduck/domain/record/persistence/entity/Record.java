40388d2c60c07447b5f39528faea7c1cc1caa531
wafla <jjangguzi@naver.com>
2025-03-09 22:54:47 +0900

    feat: 일기 엔티티 생성

---
+++ src/main/java/im/toduck/domain/record/persistence/entity/Record.java
@@ -0,0 +1,52 @@
+package im.toduck.domain.record.persistence.entity;
+
+import java.time.LocalDate;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import im.toduck.domain.user.persistence.entity.Emotion;
+import im.toduck.domain.user.persistence.entity.User;
+import im.toduck.global.base.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "record")
+@Getter
+@NoArgsConstructor
+@SQLDelete(sql = "UPDATE record SET deleted_at = NOW() where id=?")
+@SQLRestriction(value = "deleted_at is NULL")
+public class Record extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
+	@Column(nullable = false)
+	private LocalDate date; // 년월일만 저장
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Emotion emotion;
+
+	@Column(length = 50)
+	private String title;
+
+	@Column(length = 256)
+	private String img;
+
+	@Column(length = 2048)
+	private String diary;
+}
