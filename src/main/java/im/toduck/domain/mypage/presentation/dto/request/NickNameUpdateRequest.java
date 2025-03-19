d319d178d0f6494f4f181b622d1f2eae716336b2
jungbk0808 <jungbk0808@gmail.com>
2025-03-19 14:04:30 +0900

    style: 어노테이션 순서 변경
    Schema를 가장 위로 하여 validation과 명세가 분리되도록 변경

--- src/main/java/im/toduck/domain/mypage/presentation/dto/request/NickNameUpdateRequest.java
+++ src/main/java/im/toduck/domain/mypage/presentation/dto/request/NickNameUpdateRequest.java
@@ -7,8 +7,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record NickNameUpdateRequest(
-	@NotBlank(message = "닉네임은 공백일 수 없습니다.")
 	@Schema(description = "닉네임", example = "꽥꽥")
+	@NotBlank(message = "닉네임은 공백일 수 없습니다.")
 	@Pattern(regexp = NICKNAME_REGEXP, message = "올바른 닉네임을 입력해주세요.")
 	String nickname
 ) {
