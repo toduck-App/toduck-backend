0713d7c4d5c970b82deaaaeb0dc5d57357dd2e7f
wafla <jjangguzi@naver.com>
2025-03-23 12:38:10 +0900

    feat: repository 생성

---
+++ src/main/java/im/toduck/domain/concentration/presentation/repository/ConcentrationRepository.java
@@ -0,0 +1,10 @@
+package im.toduck.domain.concentration.presentation.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import im.toduck.domain.diary.persistence.entity.Diary;
+
+@Repository
+public interface ConcentrationRepository extends JpaRepository<Diary, Long> {
+}
