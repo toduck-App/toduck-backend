cc364ca72cd72f0867e589d2d019da2417a94bb8
wafla <jjangguzi@naver.com>
2025-03-28 22:46:30 +0900

    fix: json 직렬화, 역직렬화 코드 추가

--- src/main/java/im/toduck/domain/concentration/presentation/dto/request/ConcentrationRequest.java
+++ src/main/java/im/toduck/domain/concentration/presentation/dto/request/ConcentrationRequest.java
@@ -2,11 +2,15 @@ package im.toduck.domain.concentration.presentation.dto.request;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public record ConcentrationRequest(
 	@NotNull(message = "날짜는 필수 입력 항목입니다.")
+	@JsonDeserialize(using = LocalDateDeserializer.class)
 	@Schema(description = "집중 날짜", example = "2025-03-12")
 	LocalDate date,
 
