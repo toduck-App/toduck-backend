ab6fb8b9d7af57127d2e556d1849ac010cc17f82
wafla <jjangguzi@naver.com>
2025-04-03 18:43:07 +0900

    fix: swagger 어노테이션 추가

--- src/main/java/im/toduck/domain/diary/presentation/dto/response/MonthDiaryResponse.java
+++ src/main/java/im/toduck/domain/diary/presentation/dto/response/MonthDiaryResponse.java
@@ -1,6 +1,9 @@
 package im.toduck.domain.diary.presentation.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record MonthDiaryResponse(
+	@Schema(description = "특정 연월의 일기 개수 증감 (전월 대비)", example = "1")
 	Integer count
 ) {
 }
