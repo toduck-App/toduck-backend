cb45dfa134f6cf264ccb5a252062e452d5f583ba
wafla <jjangguzi@naver.com>
2025-04-07 22:39:39 +0900

    fix: 쿼리 JPQL -> QueryDSL로 수정

--- src/main/java/im/toduck/domain/concentration/persistence/repository/ConcentrationRepository.java
+++ src/main/java/im/toduck/domain/concentration/persistence/repository/ConcentrationRepository.java
@@ -1,27 +1,18 @@
 package im.toduck.domain.concentration.persistence.repository;
 
 import java.time.LocalDate;
-import java.time.YearMonth;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import im.toduck.domain.concentration.persistence.entity.Concentration;
 import im.toduck.domain.user.persistence.entity.User;
 
 @Repository
-public interface ConcentrationRepository extends JpaRepository<Concentration, Long> {
+public interface ConcentrationRepository extends JpaRepository<Concentration, Long>, ConcentrationRepositoryCustom {
 	Optional<Concentration> findByUserAndDate(User user, LocalDate date);
 
 	List<Concentration> findByUserAndDateBetween(User user, LocalDate startDate, LocalDate endDate);
-
-	@Query("SELECT AVG(c.targetCount * 100 / c.settingCount) "
-		+ "FROM Concentration c "
-		+ "WHERE c.user.id = :userId "
-		+ "AND FUNCTION('YEAR', c.date) = :#{#yearMonth.getYear()} "
-		+ "AND FUNCTION('MONTH', c.date) = :#{#yearMonth.getMonthValue()}")
-	Integer getAverageConcentrationPercentageByMonth(Long userId, YearMonth yearMonth);
 }
