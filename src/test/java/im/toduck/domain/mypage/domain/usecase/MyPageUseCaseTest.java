2a7b8c26357ef5b036280f003f617269d6f05ec5
jungbk0808 <jungbk0808@gmail.com>
2025-04-24 12:37:03 +0900

    test: 회원 탈퇴 api 테스트

--- src/test/java/im/toduck/domain/mypage/domain/usecase/MyPageUseCaseTest.java
+++ src/test/java/im/toduck/domain/mypage/domain/usecase/MyPageUseCaseTest.java
@@ -1,17 +1,31 @@
 package im.toduck.domain.mypage.domain.usecase;
 
+import static im.toduck.fixtures.user.BlockFixtures.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.SoftAssertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import im.toduck.ServiceTest;
+import im.toduck.domain.mypage.persistence.entity.AccountDeletionLog;
+import im.toduck.domain.mypage.persistence.entity.AccountDeletionReason;
+import im.toduck.domain.mypage.persistence.repository.AccountDeletionLogRepository;
 import im.toduck.domain.mypage.presentation.dto.request.NickNameUpdateRequest;
 import im.toduck.domain.mypage.presentation.dto.request.ProfileImageUpdateRequest;
+import im.toduck.domain.mypage.presentation.dto.request.UserDeleteRequest;
+import im.toduck.domain.social.persistence.entity.Social;
+import im.toduck.domain.social.persistence.repository.SocialRepository;
+import im.toduck.domain.user.persistence.entity.Block;
+import im.toduck.domain.user.persistence.entity.Follow;
 import im.toduck.domain.user.persistence.entity.User;
+import im.toduck.domain.user.persistence.repository.BlockRepository;
+import im.toduck.domain.user.persistence.repository.FollowRepository;
 import im.toduck.domain.user.persistence.repository.UserRepository;
+import im.toduck.fixtures.social.SocialFixtures;
 import im.toduck.fixtures.user.UserFixtures;
 import im.toduck.global.exception.CommonException;
 import im.toduck.global.exception.ExceptionCode;
@@ -23,6 +37,18 @@ public class MyPageUseCaseTest extends ServiceTest {
 
 	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
+	private AccountDeletionLogRepository accountDeletionLogRepository;
+
+	@Autowired
+	private FollowRepository followRepository;
+
+	@Autowired
+	private BlockRepository blockRepository;
+
+	@Autowired
+	private SocialRepository socialRepository;
 
 	@Nested
 	@DisplayName("닉네임 변경 시")
@@ -73,6 +99,67 @@ public class MyPageUseCaseTest extends ServiceTest {
 
 			// then
 			assertThat(userRepository.findById(user.getId()).get().getImageUrl()).isEqualTo(imageUrl);
+		}
+	}
+
+	@Nested
+	@DisplayName("회원 탈퇴 시")
+	class DeleteAccount {
+		private User user;
+		private UserDeleteRequest request;
+
+		@BeforeEach
+		void setUp() {
+			user = testFixtureBuilder.buildUser(UserFixtures.GENERAL_USER());
+
+			AccountDeletionReason reasonCode = AccountDeletionReason.OTHER;
+			String reasonText = "파이팅";
+			request = new UserDeleteRequest(reasonCode, reasonText);
+		}
+
+		@Test
+		public void 회원_탈퇴를_할_수_있다() {
+			// when
+			myPageUseCase.deleteAccount(user.getId(), request);
+
+			// then
+			assertSoftly(softly -> {
+				User deletedUser = userRepository.findById(user.getId()).get();
+				assertThat(deletedUser.getNickname()).isEqualTo(User.DELETED_MEMBER_NICKNAME);
+				assertThat(deletedUser.getImageUrl()).isNull();
+				assertThat(deletedUser.getDeletedAt()).isNotNull();
+
+				AccountDeletionLog log = accountDeletionLogRepository.findByUser(deletedUser).get();
+				assertThat(log.getReasonCode()).isEqualTo(request.reasonCode());
+				assertThat(log.getReasonText()).isEqualTo(request.reasonText());
+			});
+		}
+
+		@Test
+		public void 회원_탈퇴_시_공개된_관련_데이터가_삭제된다() {
+			// given
+			User followingUser = testFixtureBuilder.buildUser(UserFixtures.GENERAL_USER());
+			Follow following = testFixtureBuilder.buildFollow(user, followingUser);
+
+			User blockedUser = testFixtureBuilder.buildUser(UserFixtures.GENERAL_USER());
+			Block block = testFixtureBuilder.buildBlock(BLOCK_USER(user, blockedUser));
+
+			Social social = testFixtureBuilder.buildSocial(SocialFixtures.SINGLE_SOCIAL(user, false));
+
+			// when
+			myPageUseCase.deleteAccount(user.getId(), request);
+
+			// then
+			assertSoftly(softly -> {
+				assertThat(followRepository.findById(following.getId())).isEmpty();
+				assertThat(blockRepository.findById(block.getId())).isEmpty();
+				assertThat(socialRepository.findById(social.getId())).isEmpty();
+			});
+		}
+
+		@Test
+		public void 회원_탈퇴_시_비공개된_관련_데이터가_삭제된다() {
+			// TODO. diary, routine, concentration, schedule 관련 테스트
 		}
 	}
 }
