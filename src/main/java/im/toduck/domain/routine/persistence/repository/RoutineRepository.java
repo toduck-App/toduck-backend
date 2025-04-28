d96ac53152d5caec445fa2273c414d6cf30e1ebe
jungbk0808 <jungbk0808@gmail.com>
2025-04-28 17:20:32 +0900

    fix: 루틴 조회 시 삭제된 루틴 제외

--- src/main/java/im/toduck/domain/routine/persistence/repository/RoutineRepository.java
+++ src/main/java/im/toduck/domain/routine/persistence/repository/RoutineRepository.java
@@ -38,6 +38,6 @@ public interface RoutineRepository extends JpaRepository<Routine, Long>, Routine
 	)
 	int sumRoutineSharedCountByUser(@Param("user") User user);
 
-	@Query("SELECT r FROM Routine r WHERE r.user = :user AND r.sharedCount = 0")
+	@Query("SELECT r FROM Routine r WHERE r.user = :user AND r.sharedCount = 0 AND r.deletedAt IS null")
 	List<Routine> findAllUnsharedRoutinesByUser(@Param("user") User user);
 }
