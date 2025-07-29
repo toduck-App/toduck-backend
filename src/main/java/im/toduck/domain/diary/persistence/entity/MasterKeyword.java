cefd7ea7b8bdcc7a486d1d6d2bd85a016f86e474
wafla <jjangguzi@naver.com>
2025-07-29 15:08:04 +0900

    feat: 마스터 키워드 엔티티

---
+++ src/main/java/im/toduck/domain/diary/persistence/entity/MasterKeyword.java
@@ -0,0 +1,36 @@
+package im.toduck.domain.diary.persistence.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.Getter;
+
+@Entity
+@Table(name = "master_keywords", uniqueConstraints = {
+	@UniqueConstraint(name = "uniq_category_keyword", columnNames = {"category", "keyword"})
+})
+@Getter
+public class MasterKeyword {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private KeywordCategory category;
+
+	@Column(nullable = false, length = 255)
+	private String keyword;
+
+	@Column(name = "created_at", nullable = false)
+	private LocalDateTime createdAt;
+}
