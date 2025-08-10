04ecfedb5537f1a222d8f0371c98920d174309cb
wafla <jjangguzi@naver.com>
2025-08-10 22:47:06 +0900

    feat: diaryKeyword 테스트 코드 작성

--- src/main/java/im/toduck/domain/diary/persistence/repository/DiaryKeywordRepository.java
+++ src/main/java/im/toduck/domain/diary/persistence/repository/DiaryKeywordRepository.java
@@ -1,9 +1,12 @@
 package im.toduck.domain.diary.persistence.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import im.toduck.domain.diary.persistence.entity.DiaryKeyword;
 
 public interface DiaryKeywordRepository extends JpaRepository<DiaryKeyword, Long> {
 
+	List<DiaryKeyword> findByDiaryId(Long diaryId);
 }
