ae21b6191effca006f2f69adc6776876c163b738
wafla <jjangguzi@naver.com>
2025-08-18 18:42:45 +0900

    fix: 예외처리 추가

--- src/main/java/im/toduck/domain/diary/persistence/repository/DiaryRepositoryCustomImpl.java
+++ src/main/java/im/toduck/domain/diary/persistence/repository/DiaryRepositoryCustomImpl.java
@@ -7,16 +7,21 @@ import org.springframework.stereotype.Repository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import im.toduck.domain.diary.persistence.entity.QDiary;
+import im.toduck.global.exception.CommonException;
+import im.toduck.global.exception.ExceptionCode;
 import lombok.RequiredArgsConstructor;
 
 @Repository
 @RequiredArgsConstructor
-public class DiaryRepositoryImpl implements DiaryRepositoryCustom {
+public class DiaryRepositoryCustomImpl implements DiaryRepositoryCustom {
 
 	private final JPAQueryFactory queryFactory;
 
 	@Override
 	public LocalDate findLastDiaryDate(Long userId) {
+		if (userId == null) {
+			throw CommonException.from(ExceptionCode.NOT_FOUND_USER);
+		}
 		QDiary diary = QDiary.diary;
 
 		return queryFactory
